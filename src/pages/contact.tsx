a32474e200c450ce3a2320a4f3a1ea9c38fbe235
John Fay <john.k.fay@gmail.com>
2020-10-05 22:59:46 -0400

    contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,75 +7,101 @@ const Contact = () => (
   <Layout>
     <SEO title="Contact" />
     <div className="flex flex-col w-full items-center max-w-xs">
-      <h1 className="text-3xl">Chat with John</h1>
-      <p className="text-sm text-justify">Feel free to contact me</p>
+      <h1 className="text-3xl mb-4 text-gray-400 font-light">Chat with John</h1>
       <form
         name="contact"
         method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
         action="/success"
-        className="md:flex md:items-center w-full flex-col bg-indigo-700 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        className="flex items-center w-full flex-col bg-gray-800 shadow-lg rounded px-8 pt-6 pb-8 mb-4 max-w-screen-sm"
       >
         <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="contact" />
-        <div className="md:w-1/3">
-          <label
-            className="block text-gray-100 font-bold md:text-right mb-1 md:mb-0 pr-4"
-            htmlFor="name"
-          >
-            Name*
-          </label>
+        <div className="flex items-center mb-6">
+          <div className="w-1/3">
+            <label
+              className="block text-gray-400 font-bold md:text-right mb-1 pr-4"
+              htmlFor="name"
+            >
+              Name*
+            </label>
+          </div>
+          <div className="w-2/3">
+            <input
+              className="bg-gray-300 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-400"
+              type="text"
+              placeholder="e.g Alex Smith"
+              required
+              id="name"
+              name="name"
+            />
+          </div>
         </div>
-        <div className="md:w-2/3">
-          <input
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            type="text"
-            placeholder="e.g Alex Smith"
-            required
-            id="name"
-            name="name"
-          />
+        <div className="flex items-center mb-6">
+          <div className="w-1/3">
+            <label
+              className="block text-gray-400 font-bold md:text-right mb-1 pr-4"
+              htmlFor="email"
+            >
+              Email*
+            </label>
+          </div>
+          <div className="w-2/3">
+            <input
+              className="bg-gray-300 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-400"
+              type="email"
+              placeholder="e.g. alexsmith@gmail.com"
+              required
+              id="email"
+              name="email"
+            />
+          </div>
         </div>
-        <div className="md:w-1/3">
-          <label className="">Email*</label>
+        <div className="flex items-center mb-6">
+          <div className="w-1/3">
+            <label
+              className="block text-gray-400 font-bold md:text-right mb-1 pr-4"
+              htmlFor="phone"
+            >
+              Phone
+            </label>
+          </div>
+          <div className="w-2/3">
+            <input
+              className="bg-gray-300 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-400"
+              type="phone"
+              placeholder="111-111-1111"
+              required
+              id="phone"
+              name="phone"
+            />
+          </div>
         </div>
-        <div className="md:w-2/3">
-          <input
-            className=""
-            type="email"
-            placeholder="e.g. alexsmith@gmail.com"
-            required
-            id="email"
-            name="email"
-          />
-        </div>
-        <div className="md:w-1/3">
-          <label className="">Phone</label>
-        </div>
-        <div className="md:w-2/3">
-          <input
-            className=""
-            type="phone"
-            placeholder="111-111-1111"
-            required
-            id="phone"
-            name="phone"
-          />
-        </div>
-        <div className="md:w-1/3">
-          <label className="">Message</label>
-        </div>
-        <div className="md:w-2/3">
-          <textarea
-            className=""
-            placeholder="Hey John! I'd love to chat about working together"
-            id="message"
-            name="message"
-          />
+        <div className="flex items-center mb-6">
+          <div className="w-1/3">
+            <label
+              className="block text-gray-400 font-bold md:text-right mb-1 pr-4"
+              htmlFor="message"
+            >
+              Message
+            </label>
+          </div>
+          <div className="w-2/3">
+            <textarea
+              className="bg-gray-300 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-400"
+              placeholder="Hey John! I'd love to chat about working together"
+              id="message"
+              name="message"
+              rows={4}
+            />
+          </div>
         </div>
 
-        <button className="button is-primary" type="submit">
+        <button
+          className="mt-6 bg-purple-800 text-white py-2 px-4 rounded hover:bg-purple-700 transition-colors duration-300"
+          type="submit"
+        >
           Submit
         </button>
       </form>
